a6d9b928cc968443427c79baaf15fecf5dd327e2
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-19 16:55:16 -0400

    updated

--- algoExp/tournamentWinner.js
+++ algoExp/tournamentWinner.js
@@ -1,6 +1,6 @@
 function tournamentWinner(competitions, results) {
     let scores = {
-        "topTeam": 0
+        "topScore": 0
     }
     let bestTeam = "";
     for (const [idx, team] of competitions.entries()) {
@@ -10,8 +10,8 @@ function tournamentWinner(competitions, results) {
         if (score == 1) winner = team[0];
         if (scores[winner]) scores[winner] += 1;
         if (!scores[winner]) scores[winner] = 1;
-        if (scores[winner] > scores.topTeam) {
-            scores.topTeam = scores[winner];
+        if (scores[winner] > scores.topScore) {
+            scores.topScore = scores[winner];
             bestTeam = winner;
         }
     }
