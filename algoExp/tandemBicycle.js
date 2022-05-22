c9f0e0519544fa5b7bf0d7acc0f24888a7f9fd26
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 11:12:19 -0400

    updated

--- algoExp/tandemBicycle.js
+++ algoExp/tandemBicycle.js
@@ -1,6 +1,16 @@
-function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
-    // Write your code here.
-    return 0;
+function tandemBicycle(RSS, BSS, fastest) {
+    let sum = 0;
+    if (fastest) {
+        RSS.sort((a, b) => a - b);
+        BSS.sort((a, b) => b - a);
+    } else {
+        RSS.sort((a, b) => a - b);
+        BSS.sort((a, b) => a - b);
+    }
+    for (let idx = 0; idx < RSS.length; idx++) {
+        RSS[idx] > BSS[idx] ? sum += RSS[idx] : sum += BSS[idx];
+    }
+    return sum;
 }
 
 // Do not edit the line below.
