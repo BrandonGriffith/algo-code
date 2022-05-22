ba181885a9fddf7a6010f032ca510cf6b19664d1
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 18:44:02 -0400

    updated

--- algoExp/getNthFib.js
+++ algoExp/getNthFib.js
@@ -12,3 +12,16 @@ function getNthFib(n) {
 }
 // Do not edit the line below.
 exports.getNthFib = getNthFib;
+
+
+function getNthFib(n) {
+    const cache = [0, 1];
+    for (let i = 3; i <= n; i++) {
+        let nextFib = cache[0] + cache[1];
+        cache[0] = cache[1];
+        cache[1] = nextFib;
+    }
+    return n < 2 ? cache[0] : cache[1];
+}
+// Do not edit the line below.
+exports.getNthFib = getNthFib;
