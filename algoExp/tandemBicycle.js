eb1c9a32a357e6b50928fb79ef0c05f6392df6e5
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 11:29:11 -0400

    updated

--- algoExp/tandemBicycle.js
+++ algoExp/tandemBicycle.js
@@ -3,11 +3,8 @@ function tandemBicycle(RSS, BSS, fastest) {
     RSS.sort((a, b) => a - b);
     if (fastest) BSS.sort((a, b) => b - a);
     else BSS.sort((a, b) => a - b);
-    for (let idx = 0; idx < RSS.length; idx++) {
-        RSS[idx] > BSS[idx] ? sum += RSS[idx] : sum += BSS[idx];
-    }
+    for (let idx = 0; idx < RSS.length; idx++) RSS[idx] > BSS[idx] ? sum += RSS[idx] : sum += BSS[idx];
     return sum;
 }
-
 // Do not edit the line below.
 exports.tandemBicycle = tandemBicycle;
