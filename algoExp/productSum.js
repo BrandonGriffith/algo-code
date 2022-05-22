b5ab0971b115385e8e33faa38c022af358c6901e
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 19:33:45 -0400

    updated

--- algoExp/productSum.js
+++ algoExp/productSum.js
@@ -1,8 +1,19 @@
 // Tip: You can use the Array.isArray function to check whether an item
 // is a list or an integer.
 function productSum(array) {
-    // Write your code here.
+    let multi = 1;
+    const helper = (arr, multi) => {
+        let sum = 0;
+        for (let i of arr) {
+            if (Array.isArray(i)) {
+                sum += helper(i, multi + 1)
+            } else {
+                sum += i
+            }
+        }
+        return sum * multi;
+    }
+    return helper(array, multi);
 }
-
 // Do not edit the line below.
 exports.productSum = productSum;
