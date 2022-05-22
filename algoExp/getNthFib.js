257cbfb831ca17078e496316b0a95feea89d18f7
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 18:15:24 -0400

    updated

--- algoExp/getNthFib.js
+++ algoExp/getNthFib.js
@@ -1,6 +1,14 @@
 function getNthFib(n) {
-
+    let cache = {
+        1: 0,
+        2: 1
+    };
+    const fib = (n) => {
+        if (n in cache) return cache[n];
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+    }
+    return fib(n);
 }
-
 // Do not edit the line below.
 exports.getNthFib = getNthFib;
--- fibonacci.js
+++ fibonacci.js
@@ -1,21 +1,21 @@
 const fibonacci = (num) => {
-    return num < 2 ? num : fibonacci(num - 1) + fibonacci(num - 2);
+    return num <= 2 ? (num - 1) : fibonacci(num - 1) + fibonacci(num - 2);
 }
-console.log(fibonacci(8))
+console.log(fibonacci(6))
 
 let count = 0;
 
 const fibonacci2 = (num) => {
     count++;
-    if (num < 2) return num;
+    if (num <= 2) return num - 1;
     return fibonacci2(num - 1) + fibonacci2(num - 2);
 }
-console.log(fibonacci2(8) + " number of times run --> " + count);
+console.log(fibonacci2(6) + " number of times run --> " + count);
 
 const mainFib = () => {
     let cache = {};
     return fib = (num) => {
-        if (num < 2) return num;
+        if (num <= 2) return num - 1;
         if (num in cache) return cache[num];
         cache[num] = fib(num - 1) + fib(num - 2);
         console.log(cache)
@@ -23,4 +23,4 @@ const mainFib = () => {
     }
 }
 const fastFib = mainFib();
-console.log(fastFib(8));
+console.log(fastFib(6));
