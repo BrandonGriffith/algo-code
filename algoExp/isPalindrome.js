bc5f029907b1e1a0318d83c41af7e3e814548e07
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-23 13:41:39 -0400

    updated

--- algoExp/isPalindrome.js
+++ algoExp/isPalindrome.js
@@ -5,3 +5,29 @@ function isPalindrome(string) {
 }
 // Do not edit the line below.
 exports.isPalindrome = isPalindrome;
+
+
+
+function isPalindrome(string) {
+    let rs = "";
+    for (let i of string)
+        rs = i + rs;
+    return rs == string;
+}
+// Do not edit the line below.
+exports.isPalindrome = isPalindrome;
+
+
+
+function isPalindrome(string) {
+    let idx = 0;
+    const helper = (string, i) => {
+        let j = string.length - i - 1;
+        if (i >= j) return true;
+        if (string[i] != string[j]) return false;
+        return helper(string, i + 1);
+    }
+    return helper(string, idx);
+}
+// Do not edit the line below.
+exports.isPalindrome = isPalindrome;
