0919c2d8987aeb5143230b86bc47df80a9ceec5f
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-23 13:13:22 -0400

    updated

--- algoExp/isPalindrome.js
+++ algoExp/isPalindrome.js
@@ -1,7 +1,6 @@
 function isPalindrome(string) {
-    for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    for (let i = 0; i < Math.floor(string.length / 2); i++)
         if (string[i] != string[string.length - i - 1]) return false;
-    }
     return true;
 }
 // Do not edit the line below.
