7fad996a1ee3d666a3ee57a52339b2535789068e
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 23:34:43 -0400

    updated

--- algoExp/selectionSort.js
+++ algoExp/selectionSort.js
@@ -1,6 +1,10 @@
 function selectionSort(array) {
-    // Write your code here.
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[i])[array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    return array;
 }
-
 // Do not edit the line below.
 exports.selectionSort = selectionSort;
