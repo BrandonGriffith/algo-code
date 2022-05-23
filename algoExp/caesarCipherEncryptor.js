735251fad095970eb45df78255701c9e843f8d8e
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-23 14:21:39 -0400

    updated

--- algoExp/caesarCipherEncryptor.js
+++ algoExp/caesarCipherEncryptor.js
@@ -11,3 +11,17 @@ function caesarCipherEncryptor(string, key) {
 }
 // Do not edit the line below.
 exports.caesarCipherEncryptor = caesarCipherEncryptor;
+
+
+function caesarCipherEncryptor(string, key) {
+    const arr = [];
+    const options = "abcdefghijklmnopqrstuvwxyz";
+    key = key % 26;
+    for (let i of string) {
+        let code = options.indexOf(i) + key;
+        arr.push(options[code % 26]);
+    }
+    return arr.join('');
+}
+// Do not edit the line below.
+exports.caesarCipherEncryptor = caesarCipherEncryptor;
