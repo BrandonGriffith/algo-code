d13d5724b21e0c5d7a9b13704a7abf0365f6073d
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-23 08:41:44 -0400

    updated

--- algoExp/bubbleSort.js
+++ algoExp/bubbleSort.js
@@ -1,4 +1,4 @@
-function bubbleSort(array) {
+function bubbleSort1(array) {
     let unsorted = array.length - 1;
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < unsorted; j++) {
@@ -22,6 +22,7 @@ function bubbleSort(array) {
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];
                 sorted = false;
             }
+            count++;
         }
         if (sorted) return array;
         unsorted--;
@@ -29,3 +30,7 @@ function bubbleSort(array) {
 }
 // Do not edit the line below.
 exports.bubbleSort = bubbleSort;
+
+let count = 0;
+const arr = [9, 1, 8, 2, 7, 3, 6, 4, 5, 0, 1, 2, 3, 9, 1, 8, 2, 7, 3, 6, 4, 5, 0, 1, 2, 3];
+console.log(bubbleSort(arr), "number of loops", count);
--- algoExp/selectionSort.js
+++ algoExp/selectionSort.js
@@ -2,9 +2,13 @@ function selectionSort(array) {
     for (let i = 0; i < array.length - 1; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[i])[array[i], array[j]] = [array[j], array[i]];
+            count++;
         }
     }
     return array;
 }
 // Do not edit the line below.
 exports.selectionSort = selectionSort;
+let count = 0;
+const arr = [9, 1, 8, 2, 7, 3, 6, 4, 5, 0, 1, 2, 3, 9, 1, 8, 2, 7, 3, 6, 4, 5, 0, 1, 2, 3];
+console.log(selectionSort(arr), "number of loops", count);
