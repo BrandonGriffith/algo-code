1a7b364baed6c943b746fd24926344713bef672b
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-22 22:33:17 -0400

    updated

---
+++ algoExp/bubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(array) {
+    let unsorted = array.length - 1;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < unsorted; j++) {
+            if (array[j] > array[j + 1])[array[j], array[j + 1]] = [array[j + 1], array[j]];
+        }
+        unsorted--;
+    }
+    return array;
+}
+// Do not edit the line below.
+exports.bubbleSort = bubbleSort;
