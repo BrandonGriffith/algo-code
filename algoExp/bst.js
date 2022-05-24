8aa5a63435c0d40210f97a2c31e62b489b180f23
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-24 19:33:32 -0400

    updated

--- algoExp/bst.js
+++ algoExp/bst.js
@@ -10,21 +10,71 @@ class BST {
     }
 
     insert(value) {
-        // Write your code here.
-        // Do not edit the return statement of this method.
+        let cNode = this;
+        while (true) {
+            if (value < cNode.value) {
+                if (cNode.left == null) {
+                    cNode.left = new BST(value);
+                    break;
+                } else cNode = cNode.left;
+            } else {
+                if (cNode.right == null) {
+                    cNode.right = new BST(value);
+                    break;
+                } else cNode = cNode.right;
+            }
+        }
         return this;
     }
 
     contains(value) {
-        // Write your code here.
+        let cNode = this;
+        while (cNode) {
+            if (value < cNode.value) cNode = cNode.left;
+            else if (value > cNode.value) cNode = cNode.right;
+            else return true;
+        }
+        return false;
     }
 
-    remove(value) {
-        // Write your code here.
-        // Do not edit the return statement of this method.
+    remove(value, pNode = null) {
+        let cNode = this;
+        while (cNode) {
+            if (value < cNode.value) {
+                pNode = cNode;
+                cNode = cNode.left;
+            } else if (value > cNode.value) {
+                pNode = cNode;
+                cNode = cNode.right;
+            } else {
+                if (cNode.left && cNode.right) {
+                    cNode.value = cNode.right.getMinVal();
+                    cNode.right.remove(cNode.value, cNode);
+                } else if (!pNode) {
+                    if (cNode.left) {
+                        cNode.value = cNode.left.value;
+                        cNode.right = cNode.left.right;
+                        cNode.left = cNode.left.left;
+                    } else if (cNode.right) {
+                        cNode.value = cNode.right.value;
+                        cNode.left = cNode.right.left;
+                        cNode.right = cNode.right.right;
+                    }
+                } else if (pNode.left == cNode) {
+                    pNode.left = cNode.left ? cNode.left : cNode.right;
+                } else if (pNode.right == cNode) {
+                    pNode.right = cNode.left ? cNode.left : cNode.right;
+                }
+                break;
+            }
+        }
         return this;
     }
+    getMinVal() {
+        let cNode = this;
+        while (cNode.left) cNode = cNode.left;
+        return cNode.value;
+    }
 }
-
 // Do not edit the line below.
 exports.BST = BST;
