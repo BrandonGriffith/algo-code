fb2190cb456bd89196f6b203b63acb921d5feab8
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-28 22:26:07 -0400

    u1

---
+++ algoExp/heightBalancedBinaryTree.js
@@ -0,0 +1,16 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function heightBalancedBinaryTree(tree) {
+
+    return false;
+}
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.heightBalancedBinaryTree = heightBalancedBinaryTree;
