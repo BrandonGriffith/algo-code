2212328a50ec2be7d7c7bf54e547435d7674065c
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-29 14:36:11 -0400

    updated

--- algoExp/heightBalancedBinaryTree.js
+++ algoExp/heightBalancedBinaryTree.js
@@ -7,8 +7,24 @@ class BinaryTree {
     }
 }
 
+class TreeInfo {
+    constructor(isBalanced, height) {
+        this.isBalanced = isBalanced;
+        this.height = height;
+    }
+}
+
 function heightBalancedBinaryTree(tree) {
-    return false;
+    const helper = (node) => {
+        if (!node) return new TreeInfo(true, -1);
+        const left = helper(node.left);
+        const right = helper(node.right);
+        const isBalanced = left.isBalanced && right.isBalanced &&
+            Math.abs(left.height - right.height) <= 1;
+        const height = Math.max(left.height, right.height) + 1;
+        return new TreeInfo(isBalanced, height);
+    }
+    return helper(tree).isBalanced;
 }
 // Do not edit the lines below.
 exports.BinaryTree = BinaryTree;
