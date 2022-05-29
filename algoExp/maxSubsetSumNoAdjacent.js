604b7068f8ad67fe7980b657a1c83538660bd142
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-29 17:30:19 -0400

    updated

--- algoExp/maxSubsetSumNoAdjacent.js
+++ algoExp/maxSubsetSumNoAdjacent.js
@@ -1,6 +1,12 @@
 function maxSubsetSumNoAdjacent(array) {
-    // Write your code here.
+    if (!array.length) return 0;
+    else if (array.length == 1) return array[0];
+    const maxSums = [...array];
+    maxSums[1] = Math.max(array[0], array[1]);
+    for (let i = 2; i < array.length; i++) {
+        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+    }
+    return maxSums[maxSums.length - 1];
 }
-
 // Do not edit the line below.
 exports.maxSubsetSumNoAdjacent = maxSubsetSumNoAdjacent;
