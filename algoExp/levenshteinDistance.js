cc1ddfa0cc532286966a7419c1959fcc4a7ba53e
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-08 15:00:32 -0400

    u2

--- algoExp/levenshteinDistance.js
+++ algoExp/levenshteinDistance.js
@@ -19,6 +19,5 @@ function levenshteinDistance(str1, str2) {
     }
     return grid[str2.length][str1.length];
 }
-
 // Do not edit the line below.
 exports.levenshteinDistance = levenshteinDistance;
