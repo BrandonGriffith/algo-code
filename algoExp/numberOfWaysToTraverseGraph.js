3340aa123cd99f3c073614453a319ba0f501ac93
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-09 13:52:30 -0400

    updated

--- algoExp/numberOfWaysToTraverseGraph.js
+++ algoExp/numberOfWaysToTraverseGraph.js
@@ -1,6 +1,6 @@
 function numberOfWaysToTraverseGraph(width, height) {
-    // Write your code here.
-    return 0;
+    if (width == 1 || height == 1) return 1;
+    return numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height - 1);
 }
 // Do not edit the line below.
 exports.numberOfWaysToTraverseGraph = numberOfWaysToTraverseGraph;
