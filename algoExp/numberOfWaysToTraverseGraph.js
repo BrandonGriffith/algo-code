597aeb7f39c1abaa628318803f5a0c38056f7673
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-09 14:21:21 -0400

    updated

--- algoExp/numberOfWaysToTraverseGraph.js
+++ algoExp/numberOfWaysToTraverseGraph.js
@@ -4,3 +4,28 @@ function numberOfWaysToTraverseGraph(width, height) {
 }
 // Do not edit the line below.
 exports.numberOfWaysToTraverseGraph = numberOfWaysToTraverseGraph;
+
+
+function numberOfWaysToTraverseGraph(width, height) {
+    const ways = [];
+    for (let i = 0; i <= height; i++) {
+        ways.push([]);
+        for (let j = 0; j <= width; j++) {
+            ways[i].push(0);
+        }
+    }
+    for (let row = 1; row <= width; row++) {
+        for (let col = 1; col <= height; col++) {
+            if (row == 1 || col == 1) {
+                ways[col][row] = 1;
+            } else {
+                const left = ways[col][row - 1];
+                const up = ways[col - 1][row];
+                ways[col][row] = left + up;
+            }
+        }
+    }
+    return ways[height][width];
+}
+// Do not edit the line below.
+exports.numberOfWaysToTraverseGraph = numberOfWaysToTraverseGraph;
