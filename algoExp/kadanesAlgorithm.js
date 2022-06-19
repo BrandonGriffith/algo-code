a9e33a0258f1268d7a31467df461bdacfbd275be
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-18 23:28:35 -0400

    u5

--- algoExp/kadanesAlgorithm.js
+++ algoExp/kadanesAlgorithm.js
@@ -5,6 +5,7 @@ function kadanesAlgorithm(array) {
         maxTemp = Math.max(array[i], maxTemp + array[i]);
         maxTotal = Math.max(maxTotal, maxTemp);
     }
+
     return maxTotal;
 }
 // Do not edit the line below.
