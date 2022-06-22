09e426c376a060f950ed9f5f32999aea2e048b92
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-22 11:03:12 -0400

    u2

--- algoExp/hasSingleCycle.js
+++ algoExp/hasSingleCycle.js
@@ -1,5 +1,16 @@
 function hasSingleCycle(array) {
-    // Write your code here.
+    let visited = 0;
+    let idx = 0;
+    while (visited < array.length) {
+        if (visited > 0 && !idx) return false;
+        visited++;
+        idx = getNextIdx(idx, array);
+    }
+    return idx == 0;
+}
+const getNextIdx = (idx, array) => {
+    const nextIdx = (idx + array[idx]) % array.length;
+    return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
 // Do not edit the line below.
 exports.hasSingleCycle = hasSingleCycle;
