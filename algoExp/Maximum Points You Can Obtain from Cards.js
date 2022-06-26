d66634cda978e7ad2b793261ffcf2f967cdb9413
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-06-26 10:26:36 -0400

    u1

---
+++ algoExp/Maximum Points You Can Obtain from Cards.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+const maxScore = (C, K) => {
+    let total = 0;
+    for (let i = 0; i < K; i++) total += C[i];
+    let best = total;
+    let j = C.length - 1;
+    for (let i = K - 1; i >= 0; i--) {
+        total += C[j] - C[i], best = Math.max(best, total);
+        j--;
+    }
+    return best;
+};
+
+console.log(maxScore([1, 2, 3, 4, 5, 6, 1], 3));
