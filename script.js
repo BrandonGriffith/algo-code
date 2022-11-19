2479e9ea7717816dcef94a1abeb53fcc3a76a22a
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-11-19 14:50:07 -0500

    updated

---
+++ script.js
@@ -0,0 +1,9 @@
+let goodDay = "Friday";
+
+document.write("<p>hello world<p>");
+
+document.write("<h1>this is a test</h1>");
+
+document.write(`<h2>today is ${goodDay}</h2}`)
+
+console.log("hello world");
